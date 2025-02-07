3ba011764e190707971dc2868bf232e1b128e136
JoaoVitorSlv <joaovitordan@hotmail.com>
2025-02-07 17:54:44 -0300

    Exibindo imagens vindas da Api

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
+async function getPokemons() {
+    const consulta = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=151');
+    const result = await consulta.json();
 
+    const lista = document.getElementById('pokemon-list'); 
+
+    result.results.forEach(pokemon => {
+        const a = document.createElement('a');  
+        a.textContent = pokemon.name;           
+        a.href = pokemon.url;                   
+        a.target = "_blank";                    
+        a.classList.add('pokemon-link');        
+
+        lista.appendChild(a);
+    });
+}
+
+async function fotos() {
+    // Loop de 1 a 151
+    for (let i = 1; i <= 151; i++) {
+        const consulta = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+        const result = await consulta.json();
+        
+        const foto = result.sprites.front_default; // Foto do Pokémon (frontal)
+        
+        const pokemonCard = document.createElement('div');
+        pokemonCard.classList.add('pokemon-card');
+
+        const img = document.createElement('img');
+        img.src = foto;
+        img.alt = `Pokemon ${i}`; 
+        pokemonCard.appendChild(img);
+
+        const name = document.createElement('p');
+        name.textContent = result.name;
+        pokemonCard.appendChild(name);
+
+        document.getElementById('pokemon-list').appendChild(pokemonCard);
+    }
+}
+
+fotos()
